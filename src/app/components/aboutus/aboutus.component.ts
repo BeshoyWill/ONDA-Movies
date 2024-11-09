79f7f8c53d814c4664c94f393a719e771456a446
Beshoy William <165554296+BeshoyWill@users.noreply.github.com>
2024-11-09 21:49:42 +0200

    'edit'

--- src/app/components/aboutus/aboutus.component.ts
+++ src/app/components/aboutus/aboutus.component.ts
@@ -8,61 +8,46 @@ import { Component } from '@angular/core';
 export class AboutusComponent {
   team: any[] = [
     {
-      name: 'ziad almorsy',
-      image: './assets/team/z2.jpg',
+      name: 'Micheal',
+      image: './assets/team/member.jpg',
       gender: 'male',
     },
     {
-      name: 'Sondos Mohammed',
-      image: './assets/team/Sondos Mohammed.jpg',
-      gender: 'female',
-    },
-    {
-      name: 'Mohammed Yasser',
-      image: './assets/team/Mohammed Yasser.jpg',
+      name: 'Thomas',
+      image: './assets/team/miky.jpeg',
       gender: 'male',
     },
     {
-      name: 'Nada Hani',
-      image: './assets/team/Nada Hani.jpg',
+      name: 'Miky',
+      image: './assets/team/member.jpg',
+      gender: 'male',
+    },
+    {
+      name: 'Herr Müller',
+      image: './assets/team/miky.jpeg',
+      gender: 'male',
+    },
+
+    {
+      name: 'Meranda',
+      image: './assets/team/member.jpg',
       gender: 'female',
     },
 
     {
-      name: 'Martina Raafat',
-      image: './assets/team/Martina Raafat.jpg',
+      name: 'Amily',
+      image: './assets/team/miky.jpeg',
       gender: 'female',
     },
-
     {
-      name: 'Muaz Haitham',
-      image: './assets/team/Muaz Haitham.jpg',
+      name: 'Emma',
+      image: './assets/team/member.jpg',
+      gender: 'female',
+    },
+    {
+      name: 'Tonny',
+      image: './assets/team/miky.jpeg',
       gender: 'male',
-    },
-    {
-      name: 'Naglaa Sameh',
-      image: './assets/team/Naglaa Sameh.jpg',
-      gender: 'female',
-    },
-    {
-      name: 'Abdelrahman Nasr',
-      image: './assets/team/Abdelrahman Nasr.jpg',
-      gender: 'male',
-    },
-    {
-      name: 'Salma Mohsen',
-      image: './assets/team/Salma Mohsen.jpg',
-      gender: 'female',
-    },
-    {
-      name: 'Ali hamed',
-      image: './assets/team/Ali Hamad.jpg',
-      gender: 'male',
-    },
-    {
-      name: 'Malak Mohammed',
-      image: './assets/team/anonymous_avatars_grey_circles.jpg',
-      gender: 'female',
     },
   ];
 }
